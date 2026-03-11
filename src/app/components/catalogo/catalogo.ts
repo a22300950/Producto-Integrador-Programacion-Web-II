0aecc81c81790c2aee7b522fa7e8012012e7a3f4
a22300950 <a22300950@ceti.mx>
2026-03-11 07:39:31 -0600

    Practica 2
    Carrito

--- src/app/components/catalogo/catalogo.ts
+++ src/app/components/catalogo/catalogo.ts
@@ -1,19 +1,32 @@
-import { Component, computed } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Component, computed, signal } from '@angular/core';
+import { Product } from '../../models/producto.model';
 import { ProductsService } from '../../services/producto.service';
+import { CarritoService } from '../../services/carrito.service';
 import { ProductCardComponent } from '../product-card/product-card';
+import { CarritoComponent } from '../carrito/carrito';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, CarritoComponent],
   templateUrl: './catalogo.html',
   styleUrls: ['./catalogo.css'],
 })
 export class CatalogoComponent {
-  products;
+  products = signal<Product[]>([]);
+  inStockCount = computed(() => this.products().filter(p => p.inStock).length);
 
-  constructor(private productsService: ProductsService) {
-    this.products = toSignal(this.productsService.getAll(), { initialValue: [] });
+  constructor(
+    private productsService: ProductsService,
+    private carritoService: CarritoService
+  ) {
+    this.productsService.getAll().subscribe({
+      next: (data) => this.products.set(data),
+      error: (err) => console.error('Error cargando XML:', err),
+    });
+  }
+
+  agregar(producto: Product) {
+    this.carritoService.agregar(producto);
   }
 }
--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,15 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../models/producto.model';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [],
   templateUrl: './product-card.html',
   styleUrls: ['./product-card.css'],
 })
 export class ProductCardComponent {
   @Input({ required: true }) product!: Product;
-}
+  @Output() add = new EventEmitter<Product>();
 
+  onAdd() {
+    this.add.emit(this.product);
+  }
+}
